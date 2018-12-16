2d33f436b7e01bbff112ca41f9ca2fc3b5631d23
Meghan Sterling <sterling.meghan@gmail.com>
2018-12-16 16:08:50 -0500

    fix env variable

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const mongoose = require('mongoose')
-
 const server = require('./api/server');
 const { PORT, MONGODB_URI } = require('./api/utils/constants');
 
-server.listen(PORT, async () => {
-    await mongoose.connect(MONGODB_URI)
+server.listen(PORT, () => {
+    mongoose.connect(MONGODB_URI)
     console.log(`App listening on port ${PORT}`)
 });
